041a8cc337c614bb5db9297260a8e4f406db82f4
tpetrone <thiago.petrone@gmail.com>
2016-03-17 13:31:01 -0300

    passing all 4 cucumber scenarios

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-ruby '2.2.2'
+
+#ruby '2.2.2'
 
 gem 'sinatra', '>= 1.4'
 gem 'sinatra-flash', '0.3.0'
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,13 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
-    @word = word
+    self.word = word
+    self.guesses = ''
+    self.wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +22,49 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+
+  def guess(letter)
+
+    if letter.nil? || letter.size==0 || !(letter =~ /[a-z]/i)
+      raise ArgumentError
+    end
+
+    letter.downcase!
+    if self.guesses[letter] || self.wrong_guesses[letter]
+      return false
+    end
+
+    if word[letter]
+      self.guesses << letter
+    else
+      self.wrong_guesses << letter
+    end
+    return true
+  end
+
+
+  def word_with_guesses
+    self.word.split(//).map do |letter|
+      if guesses[letter]
+        letter
+      else
+        "-"
+      end
+    end.join
+  end
+
+
+
+  def check_win_or_lose
+
+    if self.wrong_guesses.size >= 7
+      :lose
+    elsif !word_with_guesses["-"]
+      :win
+    else
+      :play
+    end
+  end
+
 
 end
